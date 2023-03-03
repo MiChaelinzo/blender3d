d6bbac1921b2a0cbb88e17799a43fbce7d978544
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-03-03 22:49:33 +0300

    Add files via upload

---
+++ 3dblender.py
@@ -0,0 +1,93 @@
+import bpy
+import requests
+import json
+import base64
+
+# Define the DALL-E 2 prompt and generate the image
+prompt = "a cute cat playing with a ball of yarn"
+response = requests.post("https://api.openai.com/v1/images/generations", 
+                         headers={"Authorization": "Bearer <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>"}, 
+                         json={"model": "image-alpha-001", "prompt": prompt})
+
+# Check if the request was successful
+if response.status_code == 200:
+
+# Extract the image URL from the response
+    image_url = response.json()["data"][0]["url"]
+
+# Download the image and encode it as base64
+    image_content = requests.get(image_url).content
+    image_base64 = base64.b64encode(image_content).decode('ascii')
+
+# Create a new Blender scene and camera
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.delete()
+    bpy.context.scene.camera = bpy.data.cameras.new("Camera")
+    bpy.context.scene.camera_obj = bpy.data.objects.new("Camera", bpy.context.scene.camera)
+    bpy.context.scene.collection.objects.link(bpy.context.scene.camera_obj)
+
+# Create a new plane to use as the background
+    background = bpy.data.meshes.new('Plane')
+    background_obj = bpy.data.objects.new('Plane', background)
+    bpy.context.scene.collection.objects.link(background_obj)
+    bpy.context.view_layer.objects.active = background_obj
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.primitive_plane_add(size=2, enter_editmode=True)
+    bpy.ops.mesh.select_all(action='SELECT')
+    bpy.ops.mesh.delete(type='ONLY_FACE')
+    bpy.ops.object.mode_set(mode='OBJECT')
+    bpy.context.view_layer.objects.active = None
+
+# Create a new material for the background
+    background_mat = bpy.data.materials.new(name="Background")
+    background_mat.use_nodes = True
+    bsdf = background_mat.node_tree.nodes["Principled BSDF"]
+    bsdf.inputs['Base Color'].default_value = (1, 1, 1, 1)
+    output = background_mat.node_tree.nodes["Material Output"]
+    background_mat.node_tree.links.new(bsdf.outputs['BSDF'], output.inputs['Surface'])
+    background_obj.data.materials.append(background_mat)
+
+# Create a new plane to use as the image
+    image = bpy.data.meshes.new('Image')
+    image_obj = bpy.data.objects.new('Image', image)
+    bpy.context.scene.collection.objects.link(image_obj)
+    bpy.context.view_layer.objects.active = image_obj
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.primitive_plane_add(size=1, enter_editmode=True)
+    bpy.ops.mesh.select_all(action='SELECT')
+    bpy.ops.mesh.delete(type='ONLY_FACE')
+    bpy.ops.object.mode_set(mode='OBJECT')
+    bpy.context.view_layer.objects.active = None
+
+# Create a new material for the image
+    image_mat = bpy.data.materials.new(name="Image")
+    image_mat.use_nodes = True
+    image_tex = bpy.data.textures.new(name="Image", type='IMAGE')
+    image_tex.image = bpy.data.images.load(image_url)
+    tex_coord = image_mat.node_tree.nodes.new("ShaderNodeTexCoord")
+    tex_image = image_mat.node_tree.nodes.new("ShaderNodeTexImage")
+    tex_image.image = image_tex.image
+    output = image_mat.node_tree.nodes["Material Output"]
+    image_mat.node_tree.links.new(tex_coord.outputs['UV'], tex_image.inputs['Vector'])
+    image_mat.node_tree.links.new(tex_image.outputs['Color'], output.inputs['Surface'])
+    image_obj.data.materials.append(image_mat)
+
+# Position the camera and objects
+    bpy.context.scene.camera_obj.location = (0, 0, camera_distance)
+    background_obj.location = (0, 0, -camera_distance)
+    image_obj.location = (0, 0, camera_distance - 0.01) # Offset the image plane slightly to avoid z-fighting
+
+# Set the camera to look at the image plane
+    bpy.context.scene.camera_obj.rotation_mode = 'XYZ'
+    bpy.context.scene.camera_obj.rotation_euler = (0, 0, 0)
+    bpy.context.scene.camera_obj.data.lens = 50 # Set the camera focal length
+    bpy.ops.object.select_all(action='DESELECT')
+    image_obj.select_set(True)
+    bpy.context.view_layer.objects.active = image_obj
+    bpy.ops.view3d.camera_to_view_selected()
+
+# Render the scene to a file
+    bpy.context.scene.render.filepath = "C:/Users/micha/Documents/image3d.png"
+    bpy.ops.render.render(write_still=True)
+    
+
---
+++ 3dblender2.py
@@ -0,0 +1,83 @@
+import requests
+import bpy
+
+# Define the DALL-E 2 prompt and API endpoint
+prompt = "A fluffy cat sitting on a pillow"
+api_url = "https://api.openai.com/v1/images/generations"
+
+# Define the API request headers and parameters
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer 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"
+}
+params = {
+    "model": "image-alpha-001",
+    "prompt": prompt,
+    "size": "256x256",
+    "response_format": "url"
+}
+
+# Send the API request and get the response
+response = requests.post(api_url, headers=headers, json=params)
+response.raise_for_status()
+
+# Get the URL of the generated image
+image_url = response.json()["data"][0]["url"]
+
+# Create a new Blender scene
+bpy.ops.wm.read_factory_settings()
+bpy.context.scene.render.engine = 'BLENDER_EEVEE'
+bpy.context.scene.render.resolution_x = 256
+bpy.context.scene.render.resolution_y = 256
+bpy.context.scene.render.resolution_percentage = 100
+
+# Create a background plane
+background_obj = bpy.data.objects.new("Background", bpy.data.meshes.new("Background"))
+background_obj.data.vertices[0].co = (-1, -1, 0)
+background_obj.data.vertices[1].co = (1, -1, 0)
+background_obj.data.vertices[2].co = (1, 1, 0)
+background_obj.data.vertices[3].co = (-1, 1, 0)
+background_obj.data.polygons.new((0, 1, 2, 3))
+background_obj.active_material = bpy.data.materials.new(name="BackgroundMaterial")
+background_obj.active_material.diffuse_color = (1, 1, 1)
+bpy.context.scene.collection.objects.link(background_obj)
+
+# Create an image plane with the DALL-E 2 generated image
+image_data = requests.get(image_url).content
+image = bpy.data.images.load("DALL-E 2 Generated Image", bpy.context.scene.render.resolution_x, bpy.context.scene.render.resolution_y)
+image.pixels = image_data
+image.pack()
+image_obj = bpy.data.objects.new("Image", bpy.data.meshes.new("Image"))
+image_obj.data.vertices[0].co = (-1, -1, 0)
+image_obj.data.vertices[1].co = (1, -1, 0)
+image_obj.data.vertices[2].co = (1, 1, 0)
+image_obj.data.vertices[3].co = (-1, 1, 0)
+image_obj.data.polygons.new((0, 1, 2, 3))
+image_obj.active_material = bpy.data.materials.new(name="ImageMaterial")
+image_obj.active_material.use_nodes = True
+image_node = image_obj.active_material.node_tree.nodes.new("ShaderNodeTexImage")
+image_node.image = image
+image_node.location = (-200, 0)
+output_node = image_obj.active_material.node_tree.nodes["Material Output"]
+output_node.location = (200, 0)
+image_obj.active_material.node_tree.links.new(image_node.outputs["Color"], output_node.inputs["Surface"])
+bpy.context.scene.collection.objects.link(image_obj)
+
+#Scale the background and image planes to fit the camera's view
+aspect_ratio = bpy.context.scene.render.resolution_y / bpy.context.scene.render.resolution_x
+camera_distance = 1 / (2 * aspect_ratio * bpy.context.scene.camera.data.sensor_height)
+background_obj.dimensions = (aspect_ratio * camera_distance * 2, camera_distance * 2, 1)
+image_obj.dimensions = (aspect_ratio * camera_distance * 0.8, camera_distance * 0.8, 0)
+
+#Position the camera to point at the image plane
+bpy.context.scene.camera_obj.location = (0, 0, camera_distance)
+bpy.context.scene.camera_obj.rotation_mode = 'QUATERNION'
+look_at_point = image_obj.location.copy()
+look_at_point.z = 0
+camera_direction = (look_at_point - bpy.context.scene.camera_obj.location).normalized()
+bpy.context.scene.camera_obj.rotation_quaternion = camera_direction.to_track_quat('-Z', 'Y')
+
+#Render the scene to a 3D model
+bpy.ops.export_scene.obj(filepath="dalle2.obj")
+
+
